bc13ebd770b3ec22b8f2f1de2e9ce90518b8b3dc
Humberto Lucas <lukas.95_@hotmail.com>
2025-01-18 22:04:17 +0000

    1.0.1

--- example/example.js
+++ example/example.js
@@ -1,11 +1,11 @@
-const ImageApiClient = require('waifu-haven-api');
+const ImageApiClient = require('waifuhaven-api.js');
 const imageClient = new ImageApiClient('Your-Api-Key');
 const { AttachmentBuilder } = require("discord.js");
 
 imageClient.getImage('waifu', 'sfw')
     .then(base64Image => {
 
-        // Salvar a imagem como um arquivo
+        // Salva a imagem como um arquivo
 
         const buffer = Buffer.from(base64Image, 'base64');
         const attachment = new AttachmentBuilder(buffer, { name: 'image.jpg' });
